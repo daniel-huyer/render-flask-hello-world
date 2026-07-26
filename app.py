fb46185bfbf395b4983c7ff3522ab1948175dd58
daniel-huyer <dahu7022@colorado.edu>
2026-07-26 18:04:26 +0000

    8. required route /db_test

--- app.py
+++ app.py
@@ -6,3 +6,15 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello World from Daniel Huyer in 3308'
+
+@app.route("/db_test")
+def db_test():
+    conn = None
+    try:
+        conn = psycopg2.connect(DATABASE_URL)
+        return "Database connection successful"
+    except Exception as e:
+        return f"Database connection failed: {e}"
+    finally:
+        if conn is not None:
+            conn.close()
