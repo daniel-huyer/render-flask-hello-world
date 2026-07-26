eee0e5b60d49b22d984ee3b58d4b9ea00d9889f7
daniel-huyer <dahu7022@colorado.edu>
2026-07-26 18:22:49 +0000

    9. required route /db_insert and / nav

--- app.py
+++ app.py
@@ -5,7 +5,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World from Daniel Huyer in 3308'
+    return '''
+        <p>Hello World from Daniel Huyer in 3308</p>
+        <ul>
+            <li><a href="/db_test">db_test</a></li>
+            <li>a href="/db_create">db_create</a></li>
+            <li><a href="/db_insert">db_insert</a></li>
+        </ul>
+    '''
 
 @app.route("/db_test")
 def db_test():
@@ -30,6 +37,23 @@ def db_create():
             );
         """)
         return "Basketball Table Successfully Created"
+    except Exception as e:
+        return f"Database Error: {e}"
+
+
+@app.route("/db_insert")
+def db_insert():
+    try:
+        run_sql("""
+            INSERT INTO Basketball (First, Last, City, Name, Number)
+            VALUES
+            ('Jayson', 'Tatum', 'Boston', 'Celtics', 0),
+            ('Stephen', 'Curry', 'San Francisco', 'Warriors', 30),
+            ('Nikola', 'Jokic', 'Denver', 'Nuggets', 15),
+            ('Kawhi', 'Leonard', 'Los Angeles', 'Clippers', 2),
+            ('YOUR_FIRST_NAME', 'YOUR_LAST_NAME', 'CU Boulder', 'YOUR_TEAM_OR_LABEL', 3308);
+        """)
+        return "Basketball Table Populated"
     except Exception as e:
         return f"Database Error: {e}"
     
