e1cb8bc9cdb4216b2b515311d33eb0dddac56a8d
daniel-huyer <dahu7022@colorado.edu>
2026-07-26 17:45:28 +0000

    Connect Flask to PostgreSQL

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
+import os
+import psycopg2
+
 from flask import Flask
 app = Flask(__name__)
 
+DATABASE_URL = os.environ.get("DATABASE_URL")
+
 @app.route('/')
 def hello_world():
+    conn = psycopg2.connect(DATABASE_URL)
     return 'Hello World from Daniel Huyer in 3308'
